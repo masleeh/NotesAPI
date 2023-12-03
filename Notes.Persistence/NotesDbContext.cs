3fc7b1cea561e0b5d6facc150fa9db365aa362db
masleeh <102211370+masleeh@users.noreply.github.com>
2023-12-03 13:34:59 +0300

    Add project files.

---
+++ Notes.Persistence/NotesDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.App.Interfaces;
+using Notes.Domain;
+using Notes.Persistence.EntityTypeConfigurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Persistence {
+    public class NotesDbContext : DbContext, INotesDbContext {
+        public DbSet<Note> Notes { get; set; }
+        public NotesDbContext(DbContextOptions options) : base(options) {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            builder.ApplyConfiguration(new NoteConfigurations());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
